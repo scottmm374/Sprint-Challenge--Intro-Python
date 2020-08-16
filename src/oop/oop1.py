a15bbff36a4f6cd6d11ebb69618ccced930f585b
Michelle Scott <scottmm374@gmail.com>
2020-08-16 19:34:57 -0400

    oop1 pass

--- src/oop/oop1.py
+++ src/oop/oop1.py
@@ -7,6 +7,41 @@
 #   |       |
 #   v       v
 # [Car]  [Motorcycle]
+
+
+class Vehicle:  # Base class Vehicle
+    def __init__(self):
+        pass
+
+
+class GroundVehicle(Vehicle):  # Parent to Car and Motorcycle, Child to Vehicle
+    def __init__(self):
+        pass
+
+
+class Car(GroundVehicle):
+    def __init__(self):
+        pass
+
+
+class Motorcycle(GroundVehicle):
+    def __init__(self):
+        pass
+
+
+class FlightVehicle(Vehicle):  # Child of Vehicle, Parent to Airplane and Starship
+    def __init__(self):
+        pass
+
+
+class Airplane(FlightVehicle):
+    def __init__(self):
+        pass
+
+
+class Starship(FlightVehicle):
+    def __init__(self):
+        pass
 #
 # Each class can simply "pass" for its body. The exercise is about setting up
 # the hierarchy.
